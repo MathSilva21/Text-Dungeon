fe98aa8ac088d2bfe362aea54dd12d4e4d4538de
MathSilva21 <matheushssilva2001@hotmail.com>
2022-04-17 19:53:36 -0300

    Comentários

--- Modules/Interacao.lua
+++ Modules/Interacao.lua
@@ -1,7 +1,7 @@
 local Interacao = {}
 local JSON = require("json")
 
-function Pronomes(Palavra, Palavra2)
+function Pronomes(Palavra, Palavra2) -- Função para adaptar os pronomes nas frases pré feitas
     if string.sub(Palavra, #Palavra, #Palavra) == "a" then
         if string.sub(Palavra2, #Palavra2, #Palavra2) == "o" then
             Palavra2 = string.sub(Palavra2, 1, #Palavra2 - 1)
@@ -12,7 +12,7 @@ function Pronomes(Palavra, Palavra2)
     end
 end
 
-function TableTemValor(Table, Valor)
+function TableTemValor(Table, Valor) -- Checa se o que foi escrito pelo jogador existe na table
     local Valido = false
 
     for Nome, _ in pairs(Table) do
@@ -22,8 +22,8 @@ function TableTemValor(Table, Valor)
     return Valido
 end
 
-function Olhar(Resto, Ambiente)
-    local Entradas = {}
+function Olhar(Resto, Ambiente) -- Função respectiva a ação "olhar"
+    local Entradas = {} -- identifica se a entrada tem possibilidade de passagem ou não
     for Nome, Valor in pairs(Ambiente.lados) do
         if Valor.Aberto == true then
             table.insert(Entradas, Nome)
@@ -39,7 +39,7 @@ function Olhar(Resto, Ambiente)
     return "Voce esta " .. Pronomes(Ambiente.tipo, "num") .. " " .. Ambiente.tipo .. ", de tamanho " .. Ambiente.tamanho .. ".\nVoce tem entradas ao " .. table.concat(Entradas, "; ") .. ".\n" .. TextoObjetos
 end
 
-function Examinar(Resto, Ambiente)
+function Examinar(Resto, Ambiente) -- Função para examinar os objetos do ambiente (dano/defesa/coletabilidade)
     local ObjetosArquivo = io.open("Data/Objetos.json", "r")
     local Objetos = JSON.decode(ObjetosArquivo:read("*all"))
     ObjetosArquivo:close()
@@ -49,11 +49,12 @@ function Examinar(Resto, Ambiente)
     return Objetos[Resto].descricao
 end
 
-local InteracoesValidas = {
+local InteracoesValidas = { -- Array com interações válidas, podendo adicionar mais caso preciso
     ['examinar'] = Examinar,
     ['x'] = Examinar,
     ['olhar'] = Olhar,
-    ['l'] = Olhar
+    ['l'] = Olhar,
+    
 }
 
 function Interacao.InteracaoJogador(String, Ambiente)
